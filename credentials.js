979d72587ca27824bc097a1e37e89913cc3cb9a5
praveenskylark <47477031+praveenskylark@users.noreply.github.com>
2019-08-02 00:14:10 +0530

    Add files via upload

---
+++ credentials.js
@@ -0,0 +1,28 @@
+
+// Datanautix help bot
+//var accessToken = "b56ec2c85b2744ad81aeb6518d30a6ae";
+
+    //----------------------- Emoji Bot ----------------------- //
+    var credentialsAccessToken ="1f2d4c70eb62402da8b081e30d8327f9";
+
+    //var bot name is used for the firebase database
+    var credentialsBotName = "placementdetails";
+
+
+    // // HighSchoolAna(Dev)
+    // var credentialsAccessToken = "5489544adf6d490c8438cb7377f4bd60"
+
+    // var credentialsBotName = "High School Ana (Dev)"
+
+
+    var credentialsBaseUrl = "https://api.api.ai/v2/";
+
+    // Initialize Firebase
+    var credentialsConfig = {
+        apiKey: "AIzaSyA3CyIKLtJ05DXOjKIJB275sOvH8U6BaO0",
+        authDomain: "https://accounts.google.com/o/oauth2/auth",
+        databaseURL: "https://placementdetails-rpduhl.firebaseio.com",
+        projectId: "placementdetails-rpduhl",
+        storageBucket: "placementdetails-rpduhl.appspot.com",
+        messagingSenderId: "705824998394"
+    };
