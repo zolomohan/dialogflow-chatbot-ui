90d233e2c8777bfe50f40a811ef8ea3789e5493c
ZoloMohan <mohanzolomon@gmail.com>
2019-08-10 15:28:10 +0530

    Refactored function buttonResponse()

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 //PURPOSE:  Information needed to access the api.ai bot, only thing needed to be changed
 var accessToken = credentialsAccessToken,
-		botName = credentialsBotName,
-		baseUrl = credentialsBaseUrl,
-		config = credentialsConfig;
+	botName = credentialsBotName,
+	baseUrl = credentialsBaseUrl,
+	config = credentialsConfig;
 
 firebase.initializeApp(config);
 
@@ -11,9 +11,10 @@ console.log('Key for this chat instance = ' + newKey);
 
 // PURPOSE: Variables to be used for storing the last message sent and recieved for the database
 var lastSentMessage = '',
-		lastRecievedMessage = 1,
-		ButtonClicked = false,
-		DEFAULT_TIME_DELAY = 300;
+	lastRecievedMessage = 1,
+	ButtonClicked = false;
+
+const DEFAULT_TIME_DELAY = 300;
 
 // PURPOSE: Variable for the chatlogs div
 var $chatlogs = $('.chatlogs');
@@ -77,8 +78,8 @@ function send(text) {
 		},
 		data        : JSON.stringify({ query: text, lang: 'en', sessionId: 'somerandomthing' }),
 		success     : function(data) {
-			console.log("AJAX Success : ");
-			console.log(data)
+			console.log('AJAX Success : ');
+			console.log(data);
 			newRecievedMessage(JSON.stringify(data.result.fulfillment.speech, undefined, 2));
 		},
 		error       : function() {
@@ -99,7 +100,7 @@ function newRecievedMessage(messageText) {
 	// If the message contains a <ar> then it is a message whose responses are buttons
 	if (removedQuotes.includes('<ar')) {
 		buttonResponse(removedQuotes);
-	}  else {
+	} else {
 		showLoading(); // Show the typing indicator
 		setTimeout(function() {
 			createNewMessage(removedQuotes);
@@ -107,20 +108,43 @@ function newRecievedMessage(messageText) {
 	}
 }
 
+function buttonResponse(message) {
+	/*	Method called whenever an <ar> tag is found
+			The responses for this type of message will be buttons
+			This method parses out the time delays, message text and button responses
+			Then creates a new message with the time delay and creates buttons for the responses
+			Stores the matches in the message, which match the regex 
+	*/
+
+	multiMessage(message); // send the message to the multi message method to split it up, message will be sent here
+	let buttonList = message.split(/<ar>/).splice(1); // Remove the first element, The first split is the Message replied to bes displayed on the chat
+
+	var listOfInputs = [];
+	// Loop through each response and create a button
+	for (var i = 0; i < buttonList.length; i++)
+		listOfInputs.push(
+			$('<div/>', { class: 'buttonResponse' }).append($('<p/>', { class: 'chat-message', text: buttonList[i] }))
+		);
+
+	showLoading(); // Show the typing indicator
+
+	// After the time delay call the createNewMessage function
+	setTimeout(function() {
+		$('textarea').toggle(); // Hide the text area
+		$('#switchInputType').show(); // Show the switch input button
+		for (let button of listOfInputs) button.appendTo($('#buttonDiv'));
+	}, DEFAULT_TIME_DELAY);
+}
+
 function multiMessage(message) {
-	/* Method which takes messages and splits them based off a the delimeter <br 2500>
- 		The integer in the delimeter is optional and represents the time delay in milliseconds
+	/* PURPOSE: Method which takes messages and splits them based off a the delimeter <br 2500>
+ 		 The integer in the delimeter is optional and represents the time delay in milliseconds
 		 if the delimeter is not there then the time delay is set to the default 
 	*/
-	console.log('br')	
-	// Stores the matches in the message, which match the regex
-	var matches;
 
-	// List of message objects, each message will have a text and time delay
-	var listOfMessages = [];
-
-	// Regex used to find time delay and text of each message
-	var regex = /\<br(?:\s+?(\d+))?\>(.*?)(?=(?:\<br(?:\s+\d+)?\>)|$)/g;
+	var matches, // Stores the matches in the message, which match the regex
+			listOfMessages = [], // List of message objects, each message will have a text and time delay
+			regex = /\<br(?:\s+?(\d+))?\>(.*?)(?=(?:\<br(?:\s+\d+)?\>)|$)/g; // Regex used to find time delay and text of each message
 
 	// While matches are still being found in the message
 	while ((matches = regex.exec(message))) {
@@ -169,73 +193,6 @@ function multiMessage(message) {
 	})(listOfMessages, i, numMessages);
 }
 
-
-function buttonResponse(message) {
-	/*Method called whenever an <ar> tag is found
-		The responses for this type of message will be buttons
-		This method parses out the time delays, message text and button responses
-		Then creates a new message with the time delay and creates buttons for the responses
-		Stores the matches in the message, which match the regex 
-	*/
-	var matches;
-
-	// Used to store the new HTML div which will be the button
-	var $input;
-
-	// send the message to the multi message method to split it up, message will be sent here
-	multiMessage(message);
-
-	// Regex used to find time delay, text of the message and responses to be buttons
-	var regex = /\<br(?:\s+?(\d+))?\>(.*?)(?=(?:\<ar(?:\s+\d+)?\>)|$)/g;
-
-	// Seach the message and capture the groups which match the regex
-	matches = regex.exec(message);
-
-	console.log(matches);
-
-	// Create an array of the responses which will be buttons
-	var buttonList = message.split(/<ar>/);
-
-	// Remove the first element, The first split is the actual message
-	buttonList = buttonList.splice(1);
-
-	console.log(buttonList);
-
-	// Array which will store all of the newly created buttons
-	var listOfInputs = [];
-
-	// Loop through each response and create a button
-	for (var index = 0; index < buttonList.length; index++) {
-		// Store the current button response
-		var response = buttonList[index];
-
-		// Create a new div element with the text for the current button response
-		$input = $('<div/>', { class: 'buttonResponse' }).append($('<p/>', { class: 'chat-message', text: response }));
-
-		// add the new button to the list of buttons
-		listOfInputs.push($input);
-	}
-
-	// Show the typing indicator
-	showLoading();
-
-	// After the time delay call the createNewMessage function
-	setTimeout(function() {
-		// Hide the send button and the text area
-		// $('#rec').toggle();
-		$('textarea').toggle();
-
-		// Show the switch input button
-		$('#switchInputType').show();
-
-		// For each of the button responses
-		for (var index = 0; index < listOfInputs.length; index++) {
-			// Append to the chat-form div which is at the bottom of the chatbox
-			listOfInputs[index].appendTo($('#buttonDiv'));
-		}
-	}, matches[1]);
-}
-
 // Method to create a new div showing the text from API.AI
 function createNewMessage(message) {
 	// Hide the typing indicator
@@ -267,7 +224,6 @@ function createNewMessage(message) {
 
 function storeMessageToDB() {
 	var date = new Date();
-	console.log(date);
 	if (lastRecievedMessage == 1) {
 		var storeMessage = firebase.database().ref(botName).child(newKey).push({
 			UserResponse : lastSentMessage,
