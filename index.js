24528f01664810f746200716c8d52fe4eec7458c
ZoloMohan <mohanzolomon@gmail.com>
2019-08-10 16:07:43 +0530

    Fixed Textbox growing +1

--- index.js
+++ index.js
@@ -297,14 +297,12 @@ function speechResponse(message) {
 $(document)
 	.one('focus.input', 'textarea.input', function() {
 		var savedValue = this.value;
-		this.value = '';
 		this.baseScrollHeight = this.scrollHeight;
 		this.value = savedValue;
 	})
 	.on('input.input', 'textarea.input', function() {
-		var minRows = this.getAttribute('data-min-rows') | 0,
-			rows;
+		var minRows = this.getAttribute('data-min-rows') | 0, rows;
 		this.rows = minRows;
-		rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 17);
+		rows = Math.floor((this.scrollHeight - this.baseScrollHeight) / 17);
 		this.rows = minRows + rows;
 	});
