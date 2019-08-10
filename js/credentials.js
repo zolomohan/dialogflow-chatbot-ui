5b2ee4eb7cf3c9529feaf3c917f73ecf05203acd
ZoloMohan <mohanzolomon@gmail.com>
2019-08-10 16:25:23 +0530

    Refined Folder Structure

---
+++ js/credentials.js
@@ -0,0 +1,14 @@
+var credentialsAccessToken = '1f2d4c70eb62402da8b081e30d8327f9';
+var credentialsBaseUrl = 'https://api.dialogflow.com/v1/query?v=20150910';
+var credentialsBotName = 'placementdetails'; //var bot name is used for the firebase database
+
+// Credentials for Firebase
+var credentialsConfig = {
+	apiKey            : 'AIzaSyCYYLSEAmwMjPTecc0UHEzQghaeVIE0rj4',
+	authDomain        : 'placementdetails-rpduhl.firebaseapp.com',
+	databaseURL       : 'https://placementdetails-rpduhl.firebaseio.com',
+	projectId         : 'placementdetails-rpduhl',
+	storageBucket     : 'placementdetails-rpduhl.appspot.com',
+	messagingSenderId : '705824998394',
+	appId             : '1:705824998394:web:309447646d290e9f'
+};
