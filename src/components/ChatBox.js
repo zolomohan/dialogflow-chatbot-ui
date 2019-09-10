b0d8a666b7554ac6fd2d8ef33ec57a7a18c7553a
zolomohan <mohanzolomon@gmail.com>
2019-09-10 23:34:45 +0530

    Add Chat Form Component

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -2,12 +2,14 @@ import React from 'react';
 import classes from '../styles/Chat.module.css';
 import Header from './Header';
 import Logs from './Logs';
+import Form from './Form';
 
 export default function Chat({ open }) {
 	return (
 		<div className={classes.chatBox} style={{ display: open ? 'block' : 'none' }}>
 			<Header />
 			<Logs />
+			<Form />
 		</div>
 	);
 }
