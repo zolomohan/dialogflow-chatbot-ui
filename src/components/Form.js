90a9be5b5e49bcae75c473539fe8f93924500897
zolomohan <mohanzolomon@gmail.com>
2019-09-11 00:14:20 +0530

    Use inputState Hook in Chat Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import useInputState from '../hooks/useInputState';
 import classes from '../styles/Form.module.css';
 
 export default function Form() {
+	const [ text, changeText, resetText ] = useInputState();
+
+	function handleChange(event){
+		if(event.nativeEvent.inputType === 'insertLineBreak') 
+			resetText();
+		else
+			changeText(event);
+	}
+
 	return (
 		<div className={classes.chatForm}>
 			<div className={classes.chatInput}>
 				<div className={classes.suggestionDiv} />
-				<textarea className={classes.input} placeholder="Type a message" rows="1" data-min-rows="1" />
+				<textarea
+					value={text}
+					onChange={handleChange}
+					className={classes.input}
+					placeholder="Type a message"
+					rows="1"
+					data-min-rows="1"
+				/>
 			</div>
 			<div className={classes.chatFormButtons}>
 				<i className="fas fa-microphone-slash" id="speechInput" />
