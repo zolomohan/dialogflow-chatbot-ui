8c2182b8790c9062ad757170f706aff6a4c43cfc
zolomohan <mohanzolomon@gmail.com>
2019-09-10 23:02:04 +0530

    Addd Chat Box Header

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '@fortawesome/fontawesome-free/css/all.css'
+import classes from '../styles/Header.module.css';
+
+export default function Header() {
+	return (
+		<div className={classes.chatBoxHeader}>
+			<h4 className={classes.chatBotName}>Chat Bot</h4>
+			<div className={classes.icons}>
+				<i className="fas fa-volume-up" id="speechResponse" />
+				<i className="fas fa-minus chatToggle" />
+			</div>
+		</div>
+	);
+}
