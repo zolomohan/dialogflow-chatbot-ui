370f12d1c91d8d94348adc57dcbb1606706d6ef7
zolomohan <mohanzolomon@gmail.com>
2019-09-10 23:12:40 +0530

    Add Chat Logs

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import classes from '../styles/Chat.module.css';
 import Header from './Header';
+import Logs from './Logs';
 
 export default function Chat({ open }) {
 	return (
 		<div className={classes.chatBox} style={{ display: open ? 'block' : 'none' }}>
 			<Header />
+			<Logs />
 		</div>
 	);
 }
