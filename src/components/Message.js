25cf39b8c75680c4e20a376b0fdb5d6f44ccc0c3
zolomohan <mohanzolomon@gmail.com>
2019-09-10 23:24:16 +0530

    Add Message Component

---
+++ src/components/Message.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import classes from '../styles/Message.module.css';
+
+export default function Message({ text, variant }) {
+	const { chat, bot, self, botImg, chatMessage } = classes;
+	return (
+		<div className={`${chat} ${variant === 'bot' ? bot : self}`}>
+			{variant === 'bot' && (
+				<div className={botImg}>
+					<img src="https://i.ibb.co/cDCL67q/bot.png" />
+				</div>
+			)}
+			<p className={chatMessage}>{text}</p>
+		</div>
+	);
+}
