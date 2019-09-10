23cb79ef0cc3d1f4183bf8c4321fc0fa06c30546
zolomohan <mohanzolomon@gmail.com>
2019-09-11 00:24:10 +0530

    Use State for Rendering Messages

--- src/components/Logs.js
+++ src/components/Logs.js
@@ -2,14 +2,13 @@ import React from 'react';
 import Message from './Message';
 import classes from '../styles/Logs.module.css';
 
-export default function Logs() {
+export default function Logs({ messages }) {
 	return (
 		<div className={classes.chatBoxContent}>
 			<div className={classes.chatLogs}>
-				<Message
-					text="Hey I am Krypto! Say ' Hi ' to talk with me. I'll let you know the placement details of our college"
-					variant="bot"
-				/>
+				{messages.map((message) => (
+					<Message text={message.text} variant={message.variant} />
+				))}
 			</div>
 		</div>
 	);
