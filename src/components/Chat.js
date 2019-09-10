eafa33e3f2d87deb57a60b7dfc0c54e74f3245f2
zolomohan <mohanzolomon@gmail.com>
2019-09-10 23:02:04 +0530

    Addd Chat Box Header

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -3,5 +3,9 @@ import classes from '../styles/Chat.module.css';
 import Header from './Header';
 
 export default function Chat({ open }) {
-	return <div className={classes.chatBox} style={{ display: open ? 'block' : 'none' }} />;
+	return (
+		<div className={classes.chatBox} style={{ display: open ? 'block' : 'none' }}>
+			<Header />
+		</div>
+	);
 }
