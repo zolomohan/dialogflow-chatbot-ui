7b3c87757ebf91884ee02ee88a4bebf805e04d0e
zolomohan <mohanzolomon@gmail.com>
2019-09-10 23:42:07 +0530

    Use toggleState Hook to Toggle ChatBox

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,15 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment } from 'react';
+import useToggleState from '../hooks/useToggleState';
 import ChatButton from './ChatButton';
 import ChatBox from './ChatBox';
 import '../styles/App.css';
 
 function App() {
-	const [ chatBoxOpen, setchatBoxOpen ] = useState(false);
+	const [ chatBox, toggleChatBox ] = useToggleState();
 	return (
     <Fragment>
-      <ChatButton onClick={setchatBoxOpen} open={chatBoxOpen}/>
-      <ChatBox open={chatBoxOpen} />
+      <ChatButton toggleChatBox={toggleChatBox} open={chatBox}/>
+      <ChatBox toggleChatBox={toggleChatBox} open={chatBox} />
     </Fragment>
   );
 }
