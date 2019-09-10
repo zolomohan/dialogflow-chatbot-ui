3b640d9f150d5d877bc85c44785db27e070983d6
zolomohan <mohanzolomon@gmail.com>
2019-09-11 00:24:10 +0530

    Use State for Rendering Messages

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from '../styles/Chat.module.css';
 import Header from './Header';
 import Logs from './Logs';
 import Form from './Form';
 
 export default function Chat({ open, toggleChatBox }) {
+	const [ log, setLog ] = useState([
+		{
+			text:
+				"Hey I am Krypto! Say ' Hi ' to talk with me. I'll let you know the placement details of our college",
+			variant: 'bot'
+		}
+	]);
 	return (
 		<div className={classes.chatBox} style={{ display: open ? 'block' : 'none' }}>
 			<Header toggleChatBox={toggleChatBox} />
-			<Logs />
+			<Logs messages={log} />
 			<Form />
 		</div>
 	);
