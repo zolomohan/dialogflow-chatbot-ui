0e7999af84891b1dd41800628a4fa088701faf49
zolomohan <mohanzolomon@gmail.com>
2019-09-11 00:50:46 +0530

    Add key prop to Messages, Add alt prop to Images

--- src/components/Message.js
+++ src/components/Message.js
@@ -7,7 +7,7 @@ export default function Message({ text, variant }) {
 		<div className={`${chat} ${variant === 'bot' ? bot : self}`}>
 			{variant === 'bot' && (
 				<div className={botImg}>
-					<img src="https://i.ibb.co/cDCL67q/bot.png" />
+					<img src="https://i.ibb.co/cDCL67q/bot.png" alt='bot' />
 				</div>
 			)}
 			<p className={chatMessage}>{text}</p>
