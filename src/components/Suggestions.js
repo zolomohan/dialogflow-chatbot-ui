e411e2d201b89635d338c701c609cae5a8c684ab
zolomohan <mohanzolomon@gmail.com>
2019-09-11 02:51:45 +0530

    Add Use Suggestions

--- src/components/Suggestions.js
+++ src/components/Suggestions.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import classes from '../styles/Suggestions.module.css'
 
-export default function Suggestions({ suggestions }) {
+export default function Suggestions({ suggestions, handleSubmit }) {
 	return (
 		<div className={classes.suggestionContainer}>
 			{suggestions.map((suggesstion) => (
-				<div className={classes.suggestions}>
+				<div className={classes.suggestions} onClick={() => handleSubmit(suggesstion)}>
 					<p className={classes.chatMessage}>{suggesstion}</p>
 				</div>
 			))}
