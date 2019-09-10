b8ecaec0966dee587b5fc453e1af7a4079dc0e61
zolomohan <mohanzolomon@gmail.com>
2019-09-10 23:24:16 +0530

    Add Message Component

--- src/components/Logs.js
+++ src/components/Logs.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Message from './Message';
 import classes from '../styles/Logs.module.css';
 
 export default function Logs() {
 	return (
 		<div className={classes.chatBoxContent}>
-			<div className={classes.chatLogs} />
+			<div className={classes.chatLogs}>
+				<Message
+					text="Hey I am Krypto! Say ' Hi ' to talk with me. I'll let you know the placement details of our college"
+					variant="bot"
+				/>
+			</div>
 		</div>
 	);
 }
