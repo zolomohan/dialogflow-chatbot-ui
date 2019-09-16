324a76cdc39125ede893d963ea48d108c8766635
Benil Richards <mohanzolomon@gmail.com>
2019-09-16 14:06:43 +0530

    Add keys to Suggestions

--- src/components/Suggestions.js
+++ src/components/Suggestions.js
@@ -4,8 +4,8 @@ import classes from '../styles/Suggestions.module.css'
 export default function Suggestions({ suggestions, handleSubmit }) {
 	return (
 		<div className={classes.suggestionContainer}>
-			{suggestions.map((suggesstion) => (
-				<div className={classes.suggestion} onClick={() => handleSubmit(suggesstion)}>
+			{suggestions.map((suggesstion, i) => (
+				<div key={suggesstion+i} className={classes.suggestion} onClick={() => handleSubmit(suggesstion)}>
 					<p className={classes.suggestionMessage}>{suggesstion}</p>
 				</div>
 			))}
