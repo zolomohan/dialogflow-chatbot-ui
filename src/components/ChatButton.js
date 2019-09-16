61879ece3f60711c5f5045f0c03620e957b67b75
Benil Richards <mohanzolomon@gmail.com>
2019-09-16 13:30:11 +0530

    Use Icon in ChatButton

--- src/components/ChatButton.js
+++ src/components/ChatButton.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Icon from './Icon';
 import '@fortawesome/fontawesome-free/css/all.css';
 import classes from '../styles/ChatButton.module.css';
 
 export default function ChatButton({ toggleChatBox }) {
 	return (
 		<div className={classes.chatButton} onClick={toggleChatBox}>
-			<i className="fas fa-comments" />
+			<Icon type='fas' name='fa-comments' />
 		</div>
 	);
 }
