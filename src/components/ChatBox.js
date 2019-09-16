8cd351520e1a377c3c0f6bd8db0cf5f3f2bcbefa
Benil Richards <mohanzolomon@gmail.com>
2019-09-16 11:55:59 +0530

    Fix Suggestion Scroll along with TextArea

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
-import classes from '../styles/Chat.module.css';
+import $ from 'jquery';
 import Header from './Header';
 import Logs from './Logs';
+import Suggestions from './Suggestions';
 import Form from './Form';
+import classes from '../styles/Chat.module.css';
+
+const DEFAULT_TIME_DELAY = 300;
 
 export default class ChatBox extends Component {
 	state = {
@@ -21,9 +25,72 @@ export default class ChatBox extends Component {
 			log: [ ...log, { text, variant } ]
 		}));
 
-	addSuggesstion = (suggestions) => this.setState({suggestions});
+	addSuggesstion = (suggestions) => this.setState({ suggestions });
 
 	resetSuggestions = () => this.setState({ suggesstion: [] });
+
+	handleSubmit = (userResponse) => {
+		this.addMessage(userResponse, 'user');
+		this.postUserResponseToAPI(userResponse);
+	};
+
+	postUserResponseToAPI = (text) => {
+		$.ajax({
+			type: 'POST',
+			url: 'https://api.dialogflow.com/v1/query?v=20150910',
+			contentType: 'application/json; charset=utf-8',
+			dataType: 'json',
+			headers: {
+				Authorization: 'Bearer ' + '38578c4faf424691a4540abffe6a1ec8'
+			},
+			data: JSON.stringify({ query: text, lang: 'en', sessionId: 'somerandomthing' })
+		})
+			.then((data) => this.parseResponse(data.result.fulfillment.speech))
+			.catch(() => this.addMessage("it seems like there's something wrong. could you try again later?", 'bot'));
+	};
+
+	parseResponse = (responseString) => {
+		responseString = responseString.replace(/[""]/g, '');
+		if (responseString.includes('<ar')) this.suggestionResponse(responseString);
+		else if (responseString.includes('<br'))
+			setTimeout(() => this.chatResponse(responseString), DEFAULT_TIME_DELAY);
+		else this.addMessage(responseString, 'bot');
+	};
+
+	suggestionResponse = (message) => {
+		this.chatResponse(message);
+		let suggestionList = message.split(/<ar>/).splice(1);
+		this.resetSuggestions();
+		setTimeout(() => this.addSuggesstion(suggestionList), DEFAULT_TIME_DELAY);
+	};
+
+	chatResponse = (message) => {
+		var matches,
+			listOfMessages = [],
+			regex = /\<br(?:\s+?(\d+))?\>(.*?)(?=(?:\<br(?:\s+\d+)?\>)|$)/g;
+
+		while ((matches = regex.exec(message))) {
+			if (matches[1] === undefined) matches[1] = DEFAULT_TIME_DELAY;
+			var messageText = matches[2].split(/<ar>/);
+			listOfMessages.push({
+				text: messageText[0],
+				delay: matches[1]
+			});
+		}
+
+		var i = 0,
+			numMessages = listOfMessages.length;
+		const addMessage = this.addMessage;
+		(function theLoop(listOfMessages, i, numMessages) {
+			setTimeout(() => {
+				addMessage(listOfMessages[i].text, 'bot');
+				if (i++ < numMessages - 1) {
+					// showTypingIndicator();
+					theLoop(listOfMessages, i, numMessages);
+				}
+			}, listOfMessages[i].delay);
+		})(listOfMessages, i, numMessages);
+	};
 
 	render() {
 		const { open, toggleChatBox } = this.props;
@@ -31,11 +98,12 @@ export default class ChatBox extends Component {
 			<div className={classes.chatBox} style={{ display: open ? 'block' : 'none' }}>
 				<Header toggleChatBox={toggleChatBox} />
 				<Logs messages={this.state.log} />
+				<Suggestions suggestions={this.state.suggestions} handleSubmit={this.handleSubmit} />
 				<Form
-					suggestions={this.state.suggestions}
 					addMessage={this.addMessage}
 					addSuggesstion={this.addSuggesstion}
 					resetSuggestions={this.resetSuggestions}
+					handleSubmit={this.handleSubmit}
 				/>
 			</div>
 		);
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,95 +1,23 @@
 import React from 'react';
 import useInputState from '../hooks/useInputState';
-import $ from 'jquery';
-import Suggestions from './Suggestions';
 import classes from '../styles/Form.module.css';
 
-const DEFAULT_TIME_DELAY = 300;
-
-export default function Form({ addMessage, addSuggesstion, resetSuggestions, suggestions }) {
+export default function Form({ handleSubmit }) {
 	const [ text, changeText, resetText ] = useInputState();
 
 	function handleChange(event) {
-		if (event.nativeEvent.inputType === 'insertLineBreak') handleSubmit();
+		if (event.nativeEvent.inputType === 'insertLineBreak') onSubmit();
 		else changeText(event);
 	}
 
-	function handleSubmit(userResponse = text) {
-		addMessage(userResponse, 'user');
-		postUserResponseToAPI(userResponse);
+	function onSubmit(userResponse = text) {
+		handleSubmit(userResponse);
 		resetText();
-	}
-
-	function postUserResponseToAPI(text) {
-		$.ajax({
-			type: 'POST',
-			url: 'https://api.dialogflow.com/v1/query?v=20150910',
-			contentType: 'application/json; charset=utf-8',
-			dataType: 'json',
-			headers: {
-				Authorization: 'Bearer ' + '38578c4faf424691a4540abffe6a1ec8'
-			},
-			data: JSON.stringify({ query: text, lang: 'en', sessionId: 'somerandomthing' })
-		})
-			.then((data) => parseResponse(data.result.fulfillment.speech))
-			.catch(() =>
-				addMessage(
-					"it seems like there's something wrong. could you try again later?",
-					'bot'
-				)
-			);
-	}
-
-	function parseResponse(responseString) {
-		responseString = responseString.replace(/[""]/g, '');
-		if (responseString.includes('<ar')) suggestionResponse(responseString);
-		else if (responseString.includes('<br'))
-			setTimeout(() => chatResponse(responseString), DEFAULT_TIME_DELAY);
-		else addMessage(responseString, 'bot');
-	}
-
-	function suggestionResponse(message) {
-		chatResponse(message);
-		let suggestionList = message.split(/<ar>/).splice(1);
-		resetSuggestions();
-		setTimeout(() => addSuggesstion(suggestionList), DEFAULT_TIME_DELAY);
-	}
-
-	function chatResponse(message) {
-		var matches,
-			listOfMessages = [],
-			regex = /\<br(?:\s+?(\d+))?\>(.*?)(?=(?:\<br(?:\s+\d+)?\>)|$)/g;
-
-		while ((matches = regex.exec(message))) {
-			if (matches[1] === undefined) matches[1] = DEFAULT_TIME_DELAY;
-			var messageText = matches[2].split(/<ar>/);
-			listOfMessages.push({
-				text: messageText[0],
-				delay: matches[1]
-			});
-		}
-
-		var i = 0,
-			numMessages = listOfMessages.length;
-
-		(function theLoop(listOfMessages, i, numMessages) {
-			setTimeout(() => {
-				addMessage(listOfMessages[i].text, 'bot');
-				if (i++ < numMessages - 1) {
-					// showTypingIndicator();
-					theLoop(listOfMessages, i, numMessages);
-				}
-			}, listOfMessages[i].delay);
-		})(listOfMessages, i, numMessages);
 	}
 
 	return (
 		<div className={classes.chatForm}>
 			<div className={classes.chatInput}>
-				<Suggestions
-					suggestions={suggestions}
-					handleSubmit={handleSubmit}
-				/>
 				<textarea
 					value={text}
 					onChange={handleChange}
