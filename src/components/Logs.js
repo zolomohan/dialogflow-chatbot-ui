0be3690fe7bf737d7449a704b68869a953c400b0
Mohan Raj <mohanzolomon@gmail.com>
2019-09-18 11:33:58 +0530

    Add AutoScroll on Log Overflow

--- src/components/Logs.js
+++ src/components/Logs.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import Message from './Message';
 import classes from '../styles/Logs.module.css';
 
 export default function Logs({ messages, typingIndicator }) {
+	const logEnd = useRef();
+	useEffect(() => logEnd.current.scrollIntoView({ behavior: 'smooth' }));
 	return (
 		<div className={classes.chatBoxContent}>
 			<div className={classes.chatLogs}>
-				{messages.map((message, i) => (
-					<Message key={i} text={message.text} variant={message.variant} />
+				{messages.map(({ text, variant }, i) => (
+					<Message key={i} text={text} variant={variant} />
 				))}
 				{typingIndicator && (
 					<Message key="typing" variant="bot" typingIndicator />
 				)}
+				<div ref={logEnd} />
 			</div>
 		</div>
 	);
