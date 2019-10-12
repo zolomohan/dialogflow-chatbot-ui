671f74fefdff2a98f35d8514c7717da7c3c49e3b
Mohan Raj <mohanzolomon@gmail.com>
2019-10-12 18:18:14 +0530

    Modify Bot Starter Message

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React, { Fragment } from 'react';
 import useToggleState from '../hooks/useToggleState';
 import ChatButton from './ChatButton';
 import ChatBox from './ChatBox';
-import '../styles/App.css';
 
 function App() {
 	const [ chatBox, toggleChatBox ] = useToggleState(true);
--- src/helpers/starterMessage.js
+++ src/helpers/starterMessage.js
@@ -1,9 +1,10 @@
 import bot from '../config/bot';
 
 const random = (array) => array[Math.floor(Math.random() * array.length)];
-const greeting = [ 'hello', 'hey' ];
+const greeting = [ 'hello', 'hey', 'hey there', 'hello there' ];
+const intro = [ 'my name is', 'i am' ]
 
 export default {
-	text: `${random(greeting)}, my name is ${bot.name} ! what whould you like to know about?`,
+	text: `${random(greeting)}, ${random(intro)} ${bot.name} ! what would you like to know about?`,
 	user: 'bot'
 };
