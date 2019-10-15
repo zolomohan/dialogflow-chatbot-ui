05c56d26a10a2daf38f5b73b9adba47beb26778c
Mohan Raj <mohanzolomon@gmail.com>
2019-10-15 15:13:42 +0530

    Refactor Parser, Remove helper/parser - Move Content to .then() of AJAX

--- src/helpers/fetchBotResponse.js
+++ src/helpers/fetchBotResponse.js
@@ -1,31 +1,34 @@
 import $ from 'jquery';
-import parser from 'helpers/parser';
 import random from 'helpers/randomFromArray';
 import errorMessages from 'helpers/messages/error';
 import dialogflow from 'config/dialogflow';
 
 export default async (userResponse) => {
-	let botResponse = {
-		texts: [],
-		images: [],
-		suggestions: []
-	};
-
-	const { url, accessToken, sessionId } = dialogflow;
+	let response = new Object();
 	await $.post({
-		url,
+		url: dialogflow.url,
 		contentType: 'application/json; charset=utf-8',
 		dataType: 'json',
 		headers: {
-			Authorization: `Bearer ${accessToken}`
+			Authorization: `Bearer ${dialogflow.accessToken}`
 		},
 		data: JSON.stringify({
 			query: userResponse,
 			lang: 'en',
-			sessionId
+			sessionId: dialogflow.sessionId
 		})
 	})
-		.then((res) => (botResponse = parser(res.result.fulfillment.speech)))
-		.catch(() => botResponse.texts.push(random(errorMessages)));
-	return botResponse;
+		.then((res) => {
+			res = res.result.fulfillment.speech;
+			const suggestions = res.split(/<ar>/);
+			res = suggestions[0];
+			response.suggestions = suggestions.splice(1);
+
+			const texts = res.split(/<br>/);
+			res = texts[0];
+			response.texts = texts.splice(1);
+			response.images = res.split(/<img>/).splice(1);
+		})
+		.catch(() => (response.texts = [ random(errorMessages) ]));
+	return response;
 };
