c5340106a3ce1b8f856758d0b564be591244f68c
Mohan Raj <mohanzolomon@gmail.com>
2019-12-20 09:14:07 +0530

    Add Support for Custom Response Indentifiers

---
+++ src/config/resIndentifier.js
@@ -0,0 +1,5 @@
+export default{
+  text: '<br>',
+  image: '<img>',
+  suggestion: '<ar>',
+}
--- src/helpers/fetchBotResponse.js
+++ src/helpers/fetchBotResponse.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 import random from 'helpers/randomFromArray';
 import errorMessages from 'helpers/messages/error';
 import dialogflow from 'config/dialogflow';
+import resIdentifier from 'config/resIndentifier';
 
 export default async (userResponse) => {
 	let response = {};
@@ -19,15 +20,15 @@ export default async (userResponse) => {
 		})
 	})
 		.then((res) => {
-			res = res.result.fulfillment.speech;
-			const suggestions = res.split(/<ar>/);
+      res = res.result.fulfillment.speech;
+			const suggestions = res.split(new RegExp(resIdentifier.suggestion));
 			res = suggestions[0];
 			response.suggestions = suggestions.splice(1);
 
-			const texts = res.split(/<br>/);
+			const texts = res.split(new RegExp(resIdentifier.text));
 			res = texts[0];
 			response.texts = texts.splice(1);
-			response.images = res.split(/<img>/).splice(1);
+			response.images = res.split(new RegExp(resIdentifier.image)).splice(1);
 		})
 		.catch(() => (response.texts = [ random(errorMessages) ]));
 	return response;
