9b15655832bd310d3f590e6d7d38c653c7b7c490
zolomohan <mohanzolomon@gmail.com>
2020-07-17 00:44:55 +0530

    Modify for v2 API

--- src/config/dialogflow.js
+++ src/config/dialogflow.js
@@ -1,5 +1,5 @@
 export default {
-	url: 'https://api.dialogflow.com/v1/query?v=20150910',
+	url: 'http://localhost:5000/api/dialogflow/textQuery',
 	accessToken: '38578c4faf424691a4540abffe6a1ec8',
 	sessionId: 'izqGQrUr7FItDPD1XVKuVxGbiDBRNspylOSwfNfA',
 };
--- src/helpers/fetchBotResponse.js
+++ src/helpers/fetchBotResponse.js
@@ -5,31 +5,26 @@ import dialogflow from 'config/dialogflow';
 import resIdentifier from 'config/resIndentifier';
 
 export default async (userResponse) => {
-	let response = {};
-	await $.post({
-		url: dialogflow.url,
-		contentType: 'application/json; charset=utf-8',
-		dataType: 'json',
-		headers: {
-			Authorization: `Bearer ${dialogflow.accessToken}`
-		},
-		data: JSON.stringify({
-			query: userResponse,
-			lang: 'en',
-			sessionId: dialogflow.sessionId
-		})
-	})
-		.then((res) => {
-      res = res.result.fulfillment.speech;
-			const suggestions = res.split(new RegExp(resIdentifier.suggestion));
-			res = suggestions[0];
-			response.suggestions = suggestions.splice(1);
+  let response = {};
+  await $.post({
+    url: dialogflow.url,
+    contentType: 'application/json; charset=utf-8',
+    dataType: 'json',
+    data: JSON.stringify({
+      text: userResponse,
+    }),
+  })
+    .then((res) => {
+      res = res.fulfillmentText;
+      const suggestions = res.split(new RegExp(resIdentifier.suggestion));
+      res = suggestions[0];
+      response.suggestions = suggestions.splice(1);
 
-			const texts = res.split(new RegExp(resIdentifier.text));
-			res = texts[0];
-			response.texts = texts.splice(1);
-			response.images = res.split(new RegExp(resIdentifier.image)).splice(1);
-		})
-		.catch(() => (response.texts = [ random(errorMessages) ]));
-	return response;
+      const texts = res.split(new RegExp(resIdentifier.text));
+      res = texts[0];
+      response.texts = texts.splice(1);
+      response.images = res.split(new RegExp(resIdentifier.image)).splice(1);
+    })
+    .catch(() => (response.texts = [random(errorMessages)]));
+  return response;
 };
