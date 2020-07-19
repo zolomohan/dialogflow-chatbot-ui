76f9f82b1127a5090ac8227f64e368d434098103
zolomohan <mohanzolomon@gmail.com>
2020-07-20 00:45:19 +0530

    Disable Speech Features if not Available in Browser

--- src/components/chat-box/index.js
+++ src/components/chat-box/index.js
@@ -11,32 +11,43 @@ import speechConfig from 'config/speechOutput';
 import style from 'styles/ChatBox.module.css';
 
 const ChatBox = ({ open, toggleChatBox }) => {
-  const speech = new Speech(),
+  let speech, speechRecognition;
+  if (window.webkitSpeechRecognition) {
+    speech = new Speech();
     speechRecognition = new window.webkitSpeechRecognition();
 
-  speech
-    .init(speechConfig)
-    .catch((error) => console.error('An error occured while initializing Speech : ', error));
-  speechRecognition.onresult = (event) => {
-    let transcript = '';
-    for (let i = event.resultIndex; i < event.results.length; ++i)
-      transcript += event.results[i][0].transcript;
-    onUserResponse(transcript);
-    toggleSpeechInput();
-  };
+    speech
+      .init(speechConfig)
+      .catch((error) => console.error('An error occured while initializing Speech : ', error));
+    speechRecognition.onresult = (event) => {
+      let transcript = '';
+      for (let i = event.resultIndex; i < event.results.length; ++i)
+        transcript += event.results[i][0].transcript;
+      onUserResponse(transcript);
+      toggleSpeechInput();
+    };
+  }
 
   const [log, addLog] = useLogState();
   const [suggestions, setSuggestions] = useState([]);
   const [multiSelect, setMultiSelect] = useState([]);
   const [speechInput, toggleSpeechInput] = useToggleState();
   const [typing, setTyping] = useState(false);
-  const [speechOutput, toggleSpeechOutput] = useToggleState(false);
+  const [speechOutput, toggleSpeechOutput] = useToggleState(
+    window.webkitSpeechRecognition ? true : false
+  );
 
-  useEffect(speech.cancel, [speechOutput]);
-  useEffect(() => (speechInput ? speechRecognition.start() : speechRecognition.stop()), [
-    speechInput,
-    speechRecognition,
-  ]);
+  useEffect(() => {
+    if (window.webkitSpeechRecognition) speech.cancel();
+  }, [speechOutput]);
+
+  useEffect(() => {
+    if (speechInput) {
+      if (window.webkitSpeechRecognition) speechRecognition.start();
+    } else {
+      if (window.webkitSpeechRecognition) speechRecognition.stop();
+    }
+  }, [speechInput, speechRecognition]);
 
   const onUserResponse = (response) => {
     fetchBotResponse(response).then(onBotResponse);
@@ -77,6 +88,6 @@ const ChatBox = ({ open, toggleChatBox }) => {
       />
     </div>
   );
-}
+};
 
 export default ChatBox;
